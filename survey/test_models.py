198c79e84330b52d0c43e86e44de69098b7d4d6e
Blake Geno <blakegeno@gmail.com>
2015-06-26 22:49:59 -0400

    added test for employer model

--- survey/test_models.py
+++ survey/test_models.py
@@ -4,11 +4,9 @@ import models
 
 class ModeTests(TestCase):
 
-    def setUp(self):
+    def test_create_mode(self):
         models.Mode.objects.create(name="bike", met=50.0,
                                    carb=0.0, speed=20.0, green=True)
-
-    def test_create_mode(self):
         bike = models.Mode.objects.get(name='bike')
         self.assertEqual(bike.name, 'bike')
         self.assertEqual(bike.speed, 20.0)
@@ -16,20 +14,17 @@ class ModeTests(TestCase):
         self.assertEqual(bike.met, 50.0)
         self.assertEqual(bike.green, True)
 
-class LegTests(TestCase):
-
-    def setUp(self):
-        models.Mode.objects.create(name="bike", met=50.0,
-                                   carb=0.0, speed=20.0, green=True)
-        bike = models.Mode.objects.get(name='bike')
-        mode.Leg(mode=bike, duration=20, day='w')
 
 
 
-
-
-
-
+class EmployerTests(TestCase):
+    def test_create_employer(self):
+        models.Employer.objects.create(name="Co.Inc.", nr_employees=5000,
+                                       active2015=True)
+        employer = models.Employer.objects.get(name="Co.Inc.")
+        self.assertEqual(employer.name, "Co.Inc.")
+        self.assertEqual(employer.nr_employees, 5000)
+        self.assertTrue(employer.active2015)
 
 
 
